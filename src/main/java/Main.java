bc6b8e6ef213e6fd29feb050bbf730d6e29bad43
azaad3100 <azaad.ramkisoen3100@gmail.com>
2024-01-24 21:37:09 -0300

    Final backend prodcut

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,6 @@ import jakarta.persistence.EntityManager;
 import service.*;
 
 public class Main {
-
-
     private static EntityManager entityManager = JPAConfiguration.getEntityManager();
     public static void main(String[] args) {
         entityManager.getTransaction().begin();
